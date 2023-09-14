49a3fdae3b9ea67d2d027ef5cc12d0eb3e6285a0
Donavan <130393961+DonnieFunkie@users.noreply.github.com>
2023-09-14 10:53:30 -0400

    Updated Player Controler Script

--- Git Demo/Assets/Scripts/PlayerControler.cs
+++ Git Demo/Assets/Scripts/PlayerControler.cs
@@ -17,6 +17,7 @@ public class PlayerControler : MonoBehaviour
     private CharacterController characterController;
     private Vector2 moveInput;
     private Vector3 playerPosition;
+    private Vector3 playerVelocity;
     private bool grounded;
     private Vector2 mouseMovement;
 
@@ -29,24 +30,74 @@ public class PlayerControler : MonoBehaviour
     void Start()
     {
         
+        characterController = GetComponent<CharacterController>();
+
+        //Hides the mouse cursir at start
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        grounded = characterController.isGrounded;
+        MovePlayer();
+
+    }
+
+    public void MovePlayer()
+    {
+        //Direction is Move
+        Vector3 moveVec = transform.right * moveInput.x + transform.forward * moveInput.y;
+
+        //Move Controller
+        characterController.Move(moveVec * speed * Time.deltaTime);
+
+        playerVelocity.y += gravity * Time.deltaTime;
+
+        if (grounded && playerVelocity.y < 0) 
+            {
+                playerVelocity.y = -2.5f;
         
+            }
+        characterController.Move(playerVelocity * Time.deltaTime);
+    }
+
+    public void Look() 
+    { 
+        float xAmount = mouseMovement.x * sensitivity * Time.deltaTime;
+        float yAmount = mouseMovement.y * sensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseMovement * sensitivity * Time.deltaTime);
+
+        cam_x_rotation -= xAmount;
+        cam_x_rotation = Mathf.Clamp(cam_x_rotation, -90f, 90f);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log("Move Input Value" + moveInput.ToString());
 
     }
     public void OnLook(InputAction.CallbackContext context)
     {
-
+        mouseMovement = context.ReadValue<Vector2>();
+        Debug.Log("Mouse Movement Value" + mouseMovement.ToString());
     }
     public void OnJump(InputAction.CallbackContext context)
-    { 
-   
+    {
+
+        Jump();
+        Debug.Log("Jump Input Value" + moveInput.ToString());
     }
+
+    public void Jump()
+    {
+        if (grounded)
+        {
+            playerVelocity.y = jumpForce;
+        }
+    }
+
 }
